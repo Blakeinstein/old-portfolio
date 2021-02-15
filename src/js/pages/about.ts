09b5f9c9200394f0f8585492e4edb91d20be968b
Blaine <rishi1998@gmail.com>
2021-02-15 13:09:01 +0530

    Implement about page with tooltips

--- src/js/pages/about.ts
+++ src/js/pages/about.ts
@@ -1,4 +1,4 @@
-import {html} from 'lit-html';
+import { html } from 'lit-html';
 
 import stars from "url:../../assets/logo.svg";
 
@@ -11,16 +11,22 @@ let aboutRender = () => {
             <p>I'm a developer, software engineer and a tech enthusiast. I love to try new things, explore possibilites and anything that amuses my curiosity. I am currently pursuing a degree in Computer Science and Engineering at, National Institute of Technology, Delhi.</p>
             <h4><i class="fas fa-angle-down"></i> Languages and Frameworks</h4>
             <ul class="languages">
-                <li>Rust</li>
-                <li>Javascript/Typescript</li>
-                <li>Python</li>
-                <li>C/C++</li>
-                <li>others..</li>
+                <li> <div data-tippy-content="Serde, WebViewRS/Tauri, Rocket...">Rust</div></li>
+                <li> <div data-tippy-content="React, ThreeJS, GSAP, JQuery">Javascript/Typescript</div></li>
+                <li> <div data-tippy-content="PyQT5/PySide2, FBS, Pandas">Python</div></li>
+                <li> <div data-tippy-content="Qt5, Boost">C/C++</div></li>
+                <li> <div data-tippy-content="MATLAB, Latex, Julia, GoLang, Sass, WASM, ASM8086 and ASM6402">others..</div></li>
+            </ul>
+            <h4><i class="fas fa-angle-down"></i> Skillsets</h4>
+            <ul class="Skillsets">
+                <li>Software Engineering</li>
+                <li> <div data-tippy-content="Docker, Vim, Bash, SSH, Git">Devtools</div></li>
+                <li> <div data-tippy-content="After Effects, Photoshop, Illustrator...">Adobe Suite</div></li>
             </ul>
             <h4><i class="fas fa-angle-down"></i> Experience</h4>
             <ul class="experience">
-                <li>Intern at FOSSEE, IIT Bombay</li>
-                <li>Executive Member at UpVision, Tech club of NIT Delhi</li>
+                <li> <div data-tippy-content="<a href=''>Github</a>, <a href=''>Certificate</a>, <a href=''>Report</a>">Intern at FOSSEE, IIT Bombay</div></li>
+                <li> <div data-tippy-content="<a href=''>Github</a>, <a href=''>Website</a>">Executive Member at UpVision, Tech club of NIT Delhi</div></li>
             </ul>
             <div class="about-links">
                 <a class="ghostButton" href="https://drive.google.com/file/d/1MQjMb-XB-TLvx1LrGUSgP6O0sfBEodCF/view?usp=sharing">Resume</a>
