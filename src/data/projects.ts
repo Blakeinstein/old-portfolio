78d14366be9c320f740bd0fd75c2eb8381d23f05
Blaine <rishi1998@gmail.com>
2021-02-18 17:42:00 +0530

    switch to ts file instead of json

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,22 +1,26 @@
-[{
+import * as bloop from "url:../assets/projects/bloop.png";
+import * as fuse from "url:../assets/projects/fuse.png";
+import * as pfd from "url:../assets/projects/pfd.png";
+
+export default [{
     "name": "Bloop",
     "description": "An Aesthetic Scratchpad for developers.",
     "lang": ["Rust", "HTML", "CSS", "JS", "C/C++"],
     "libs": ["WebViewRS", "Fuse", "Serde", "WinAPI"],
     "url": "https://github.com/Blakeinstein/Bloop",
-    "image": "https://i.imgur.com/iS2aPT6.png"
+    "image": bloop
 },{
     "name": "Fuse-Rust",
     "description": "A fuzzy searching library written in rust.",
     "lang": ["Rust"],
     "libs": ["crossbeam"],
     "url": "https://github.com/Blakeinstein/fuse-rust",
-    "image": "https://i.imgur.com/Vw2QQMe.png"
+    "image": fuse
 },{
     "name": "ChemicalPFD",
     "description": "Tool to construct process flow diagrams",
     "lang": ["Python"],
     "libs": ["PyQT5", "PySide2", "FBS"],
     "url": "https://github.com/FOSSEE/Chemical-PFD",
-    "image": "https://i.imgur.com/NyxJ1Sk.png"
+    "image": pfd
 }]
