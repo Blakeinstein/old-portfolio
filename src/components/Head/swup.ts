963eeff9528ca291da34895000952f06e80e5299
Blaine <rishi1998@gmail.com>
2021-09-01 22:12:25 +0530

    add cute comment

--- src/components/Head/swup.ts
+++ src/components/Head/swup.ts
@@ -2,6 +2,8 @@ import Swup from 'swup';
 import SwupSlideTheme from "@swup/slide-theme";
 import SwupHeadPlugin from '@swup/head-plugin';
 
+console.log("🚀 Coded with ❤ by Blaine.")
+
 const swup = new Swup({
   plugins: [ new SwupSlideTheme(), new SwupHeadPlugin({
     "persistAssets": true
