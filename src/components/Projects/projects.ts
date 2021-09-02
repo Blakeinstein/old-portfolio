47d742c29861196c031249e3f29e3c87447375b9
Blaine <rishi1998@gmail.com>
2021-09-03 00:31:44 +0530

    Fix path issues

--- src/components/Projects/projects.ts
+++ src/components/Projects/projects.ts
@@ -11,19 +11,19 @@ export default <{
   "lang": ["Rust", "HTML", "CSS", "JS", "C/C++"].join(", "),
   "libs": ["WebViewRS", "Fuse", "Serde", "WinAPI"].join(", "),
   "url": "https://github.com/Blakeinstein/Bloop",
-  "image": "assets/projects/bloop.png"
+  "image": "/assets/projects/bloop.png"
 },{
   "name": "Fuse-Rust",
   "description": "A fuzzy searching library written in rust.",
   "lang": ["Rust"].join(", "),
   "libs": ["crossbeam"].join(", "),
   "url": "https://github.com/Blakeinstein/fuse-rust",
-  "image": "assets/projects/fuse.png"
+  "image": "/assets/projects/fuse.png"
 },{
   "name": "ChemicalPFD",
   "description": "Tool to construct process flow diagrams",
   "lang": ["Python"].join(", "),
   "libs": ["PyQT5", "PySide2", "FBS"].join(", "),
   "url": "https://github.com/FOSSEE/Chemical-PFD",
-  "image": "assets/projects/pfd.png"
+  "image": "/assets/projects/pfd.png"
 }]
