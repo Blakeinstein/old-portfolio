c44a8de87167f541f4e6c92885de5f30ca50e75d
Blaine <rishi1998@gmail.com>
2021-09-03 17:36:25 +0530

    Fix contact form transition

--- src/components/Contact/contact.ts
+++ src/components/Contact/contact.ts
@@ -5,72 +5,74 @@ init("user_wird1j6wAcU6lj00aaLo9");
 
 let isSending = false;
 
-function contactMain() {
-	document.getElementById('noSpam').oninvalid = () => {
-		console.log("Spammer not in the checked file. This incident will be reported.");
-		tippy("#noSpamDiv", {
-			content: "Spammer not in the checked file. This incident will be reported.",
-			inertia: true,
-			arrow: roundArrow,
-			placement: "top",
-			showOnCreate: true,
-			trigger: "manual",
-		});
-	}
+function invalidSpam () {
+	console.log("Spammer not in the checked file. This incident will be reported.");
+	tippy("#noSpamDiv", {
+		content: "Spammer not in the checked file. This incident will be reported.",
+		inertia: true,
+		arrow: roundArrow,
+		placement: "top",
+		showOnCreate: true,
+		trigger: "manual",
+	});
+}
 
-	document.getElementById('contactForm').addEventListener('submit', (e: Event) => {
-		e.preventDefault();
+function contactSubmit (e: Event) {
+	e.preventDefault();
 
-		if (isSending) return false;
+	if (isSending) return false;
 
-		let FD = new FormData(
-			document.getElementById("contactForm") as HTMLFormElement,
-		);
-		let params = {
-			name: FD.get("name"),
-			email: FD.get("email"),
-			subject: FD.get("subject"),
-			message: FD.get("message"),
-		};
+	let FD = new FormData(
+		document.getElementById("contactForm") as HTMLFormElement,
+	);
+	let params = {
+		name: FD.get("name"),
+		email: FD.get("email"),
+		subject: FD.get("subject"),
+		message: FD.get("message"),
+	};
 
-		isSending = true;
-		let proceed = true;
-		send("main", "base", params).then(
-			res => {
-				if (res.status == 200) {
-					document
-						.querySelector("#contactForm")
-						.classList.add("slow-fade-out");
-					document
-						.querySelector("#complete")
-						.classList.add("slow-fade-in");
-				} else {
-					tippy("#submit", {
-						content: `${res.status}: ${res.text}`,
-						inertia: true,
-						arrow: roundArrow,
-						placement: "top",
-						showOnCreate: true,
-						trigger: "manual",
-					});
-					proceed = false;
-				}
-				isSending = false;
-			},
-			err => {
+	isSending = true;
+	let proceed = true;
+	send("main", "base", params).then(
+		res => {
+			console.log(res);
+			if (res.status == 200) {
+				document
+					.querySelector(".formBox")
+					.classList.add("complete");
+			} else {
 				tippy("#submit", {
-					content: `${err}`,
+					content: `${res.status}: ${res.text}`,
 					inertia: true,
 					arrow: roundArrow,
 					placement: "top",
 					showOnCreate: true,
 					trigger: "manual",
 				});
-				isSending = false;
-			},
-		);
-		return proceed;
-	});
+				proceed = false;
+			}
+			isSending = false;
+		},
+		err => {
+			tippy("#submit", {
+				content: `${err}`,
+				inertia: true,
+				arrow: roundArrow,
+				placement: "top",
+				showOnCreate: true,
+				trigger: "manual",
+			});
+			isSending = false;
+		},
+	);
+	return proceed;
+}
+
+function contactMain() {
+	document.getElementById('noSpam').oninvalid = invalidSpam;
+
+	document.getElementById('contactForm').onsubmit = contactSubmit;
 }
 
 export default contactMain;
