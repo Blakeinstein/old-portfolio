13ae94ed22793083e1103a54e6a043e2a9532242
Blaine <rishi1998@gmail.com>
2021-09-03 13:55:01 +0530

    FIx contact form validation, use native forms for lighthouse.

--- src/components/Contact/contact.ts
+++ src/components/Contact/contact.ts
@@ -5,23 +5,22 @@ init("user_wird1j6wAcU6lj00aaLo9");
 
 let isSending = false;
 
-document.querySelector('.submit>a').addEventListener('click', (e: Event) => {
+document.getElementById('noSpam').oninvalid = () => {
+	console.log("Checkbox not in the checked file. This incident will be reported.");
+	tippy("#noSpamDiv", {
+		content: "I am glad you are honest about being a spammer.",
+		inertia: true,
+		arrow: roundArrow,
+		placement: "top",
+		showOnCreate: true,
+		trigger: "manual",
+	});
+}
 
-	console.log("Handling submit");
+document.getElementById('contactForm').addEventListener('submit', (e: Event) => {
 	e.preventDefault();
 
 	if (isSending) return false;
-	if (!(document.getElementById("noSpam") as HTMLInputElement).checked) {
-		tippy("#noSpamDiv", {
-			content: "I am glad you are honest about being a spammer.",
-			inertia: true,
-			arrow: roundArrow,
-			placement: "top",
-			showOnCreate: true,
-			trigger: "manual",
-		});
-		return false;
-	}
 
 	let FD = new FormData(
 		document.getElementById("contactForm") as HTMLFormElement,
@@ -33,57 +32,41 @@ document.querySelector('.submit>a').addEventListener('click', (e: Event) => {
 		message: FD.get("message"),
 	};
 
+	isSending = true;
 	let proceed = true;
-	for (let key in params) {
-		if (params[key].trim() == "") {
-			tippy(`#${key}>label`, {
-				content: "Field cannot be empty!",
-				inertia: true,
-				arrow: roundArrow,
-				placement: "top",
-				showOnCreate: true,
-				trigger: "manual",
-			});
-			proceed = false;
-		}
-	}
-
-	if (proceed) {
-		isSending = true;
-		send("main", "base", params).then(
-			res => {
-				if (res.status == 200) {
-					document
-						.querySelector("#contactForm")
-						.classList.add("slow-fade-out");
-					document
-						.querySelector("#complete")
-						.classList.add("slow-fade-in");
-				} else {
-					tippy("#submit", {
-						content: `${res.status}: ${res.text}`,
-						inertia: true,
-						arrow: roundArrow,
-						placement: "top",
-						showOnCreate: true,
-						trigger: "manual",
-					});
-					proceed = false;
-				}
-				isSending = false;
-			},
-			err => {
+	send("main", "base", params).then(
+		res => {
+			if (res.status == 200) {
+				document
+					.querySelector("#contactForm")
+					.classList.add("slow-fade-out");
+				document
+					.querySelector("#complete")
+					.classList.add("slow-fade-in");
+			} else {
 				tippy("#submit", {
-					content: `${err}`,
+					content: `${res.status}: ${res.text}`,
 					inertia: true,
 					arrow: roundArrow,
 					placement: "top",
 					showOnCreate: true,
 					trigger: "manual",
 				});
-				isSending = false;
-			},
-		);
-	}
+				proceed = false;
+			}
+			isSending = false;
+		},
+		err => {
+			tippy("#submit", {
+				content: `${err}`,
+				inertia: true,
+				arrow: roundArrow,
+				placement: "top",
+				showOnCreate: true,
+				trigger: "manual",
+			});
+			isSending = false;
+		},
+	);
 	return proceed;
 });
